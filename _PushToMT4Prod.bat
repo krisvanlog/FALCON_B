ee255bddead433c7b9710cb330b5e54cfc301b1e
krisvanlog <66163203+krisvanlog@users.noreply.github.com>
2020-06-29 10:07:19 -0700

    ModifiedForMacBookAir
    ModifiedForMacBookAir

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,11 +4,11 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\FALCON_B"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\04_FALCON_B"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\04_FALCON_B"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\04_FALCON_B"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\04_FALCON_B"
+set SOURCE_DIR="C:\Users\Kris\Documents\GitHub\LazyTrade\FALCON_B"
+set DEST_DIR1="C:\Program Files (x86)\MetaTrader - AxiTrader - Term1\MQL4\Experts\01_FALCON_B"
+set DEST_DIR2="C:\Program Files (x86)\MetaTrader - AxiTrader - Term2\MQL4\Experts\02_FALCON_B"
+set DEST_DIR3="C:\Program Files (x86)\MetaTrader - AxiTrader - Term3\MQL4\Experts\03_FALCON_B"
+set DEST_DIR4="C:\Program Files (x86)\MetaTrader - AxiTrader - Term4\MQL4\Experts\04_FALCON_B"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
